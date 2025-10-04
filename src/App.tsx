2b7961fe91ed000e2a2cd0fa874595e0c60bef54
Andrew Hessler <adhessler@gmail.com>
2025-10-04 07:53:09 -0500

    add an all picker

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,25 @@ const ROUND_1_OPTIONS = ["mario bros' circuit", "crown city", "desert hills", "k
 const ROUND_2_OPTIONS = ["wario stadium", "shy guy bazaar", "sky-high sundae", "dino dino jungle", "starview peak", "dandelion depths", "boo cinema", "peach beach"];
 const ROUND_3_OPTIONS = ["peach stadium", "airstrip fortress", "cheep cheep falls", "dry bones burnout", "acorn heights", "faraway oasis", "great ? block ruins", "toad's factory"];
 const ROUND_4_OPTIONS = ["salty salty speedway", "bowser's castle", "choco mountain", "mario circuit", "wario shipyard", "rainbow road", "dk pass"];
+const ALL_OPTIONS = [...ROUND_1_OPTIONS, ...ROUND_2_OPTIONS, ...ROUND_3_OPTIONS, ...ROUND_4_OPTIONS];
 const ROUND_OPTIONS = [ROUND_1_OPTIONS, ROUND_2_OPTIONS, ROUND_3_OPTIONS, ROUND_4_OPTIONS];
+const ROUND_ALL_OPTIONS = [ALL_OPTIONS, ALL_OPTIONS, ALL_OPTIONS, ALL_OPTIONS];
+
 
 const calcResults = () => ROUND_OPTIONS.map((options) =>
   options[Math.floor(Math.random() * options.length)]
 )
+
+const calcAllResults = () => {
+  const picked: number[] = [];
+  const result = ROUND_ALL_OPTIONS.map((options) => {
+    const choice = Math.floor(Math.random() * options.length);
+    while (!picked.includes(choice)) {
+      return options[choice]
+    }
+  });
+  return result
+}
 
 
 function App() {
@@ -17,7 +31,11 @@ function App() {
 
   const randomizeResults = useCallback(() => {
     setRoundResults(calcResults());
-  }, [])
+  }, []);
+
+  const randomizeAllResults = useCallback(() => {
+    setRoundResults(calcAllResults());
+  }, []);
 
   return (
     <>
@@ -34,6 +52,7 @@ function App() {
         )}
       </div >
       <button onClick={randomizeResults}>Randomize Tracks</button>
+      <button onClick={randomizeAllResults}>Randomize All Tracks</button>
     </>
   )
 }
