29455c9ce14cac037a303f4f6111247fd445a7dd
Andrew Hessler <adhessler@gmail.com>
2025-10-21 18:01:28 -0500

    fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 
 const ROUND_1_OPTIONS = ["mario bros' circuit", "crown city", "desert hills", "koopa troopa beach", "moo moo meadows", "dk spaceport", "whistlestop summit"];
 const ROUND_2_OPTIONS = ["wario stadium", "shy guy bazaar", "sky-high sundae", "dino dino jungle", "starview peak", "dandelion depths", "boo cinema", "peach beach"];
-const ROUND_3_OPTIONS = ["peach stadium", "airstrip fortress", "cheep cheep falls", "dry bones burnout", "acorn heights", "faraway oasis", "great ? block ruins", "toad's factory"];
+const ROUND_3_OPTIONS = ["peach stadium", "airship fortress", "cheep cheep falls", "dry bones burnout", "acorn heights", "faraway oasis", "great ? block ruins", "toad's factory"];
 const ROUND_4_OPTIONS = ["salty salty speedway", "bowser's castle", "choco mountain", "mario circuit", "wario shipyard", "rainbow road", "dk pass"];
 const ALL_OPTIONS = [...ROUND_1_OPTIONS, ...ROUND_2_OPTIONS, ...ROUND_3_OPTIONS, ...ROUND_4_OPTIONS];
 const ROUND_OPTIONS = [ROUND_1_OPTIONS, ROUND_2_OPTIONS, ROUND_3_OPTIONS, ROUND_4_OPTIONS];
